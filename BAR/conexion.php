240d49c0e7879467927d866d826175610dbf9a8f
Nataly2802 <wendynatalyestevezlopez@gmail.com>
2025-10-28 16:11:26 -0500

    Update conexion.php

--- BAR/conexion.php
+++ BAR/conexion.php
@@ -1,5 +1,8 @@
 <?php
-$conexion = new mysqli("localhost", "usuariobar", "BarNataly", "bar");
+require_once 'C:/config/config.php';
+
+$conexion = new mysqli($db_host, $db_user, $db_pass, $db_name);
+
 if ($conexion->connect_error) {
     die("Error en la conexión: " . $conexion->connect_error);
 }
