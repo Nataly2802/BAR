7d9fe2aa8810cd559076520554e648d194c4ba23
Nataly2802 <wendynatalyestevezlopez@gmail.com>
2025-10-29 12:43:54 -0500

    Update conexion.php

--- BAR/conexion.php
+++ BAR/conexion.php
@@ -1,13 +1,20 @@
 <?php
 namespace App;
 
-require_once 'C:/config/config.php';
+use Config\Config; 
 
 class Conexion {
     public $conexion;
 
     public function __construct() {
-        $this->conexion = new \mysqli($GLOBALS['db_host'], $GLOBALS['db_user'], $GLOBALS['db_pass'], $GLOBALS['db_name']);
+        $config = new Config();
+        $this->conexion = new \mysqli(
+            $config->db_host,
+            $config->db_user,
+            $config->db_pass,
+            $config->db_name
+        );
+
         if ($this->conexion->connect_error) {
             die("Error en la conexión: " . $this->conexion->connect_error);
         }
